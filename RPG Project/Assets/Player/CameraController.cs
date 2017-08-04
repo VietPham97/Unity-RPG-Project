03d962b08d6dfb505ef1fbc1297da14396c1393b
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-08-03 23:06:11 -0400

    Make the camera follow the player in runtime.

---
+++ RPG Project/Assets/Player/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+    public Transform target; // Player
+
+    public Vector3 offset;
+    public float pitch = 2f;
+
+    float currentZoom = 10f;
+
+    private void LateUpdate()
+    {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+    }
+}
