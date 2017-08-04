fa73de22356f4746f87c8c4bd84585007bb809de
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-08-03 23:11:34 -0400

    Use A and D key to rotate the camera around the player.

--- RPG Project/Assets/Player/CameraController.cs
+++ RPG Project/Assets/Player/CameraController.cs
@@ -5,22 +5,29 @@ public class CameraController : MonoBehaviour
     public Transform target; // Player
 
     public Vector3 offset;
-    public float pitch = 2f;
     public float zoomSpeed = 4f;
     public float minZoom = 5f;
     public float maxZoom = 15f;
 
-    float currentZoom = 10f;
+	public float pitch = 2f;
+    public float yawSpeed = 100f;
+
+	float currentZoom = 10f;
+    float currentYaw;
 
     private void Update()
     {
         currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
         currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
     }
 
     private void LateUpdate()
     {
         transform.position = target.position - offset * currentZoom;
         transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
     }
 }
