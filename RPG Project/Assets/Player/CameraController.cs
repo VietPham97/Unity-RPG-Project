dd738456172cf08ecdeb54bfbab30b402b7e391c
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-08-03 23:07:57 -0400

    Add zoom in/out function to the camera.

--- RPG Project/Assets/Player/CameraController.cs
+++ RPG Project/Assets/Player/CameraController.cs
@@ -6,8 +6,17 @@ public class CameraController : MonoBehaviour
 
     public Vector3 offset;
     public float pitch = 2f;
+    public float zoomSpeed = 4f;
+    public float minZoom = 5f;
+    public float maxZoom = 15f;
 
     float currentZoom = 10f;
+
+    private void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+    }
 
     private void LateUpdate()
     {
