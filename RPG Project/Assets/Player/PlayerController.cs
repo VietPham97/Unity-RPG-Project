2981c7af54103536b744681fc0ed060667901e7e
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-08-03 22:45:45 -0400

    Detect mouse click position via ray every frame.

---
+++ RPG Project/Assets/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+    public LayerMask movementMask;
+    Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+                // TODO Move player to the hit point
+
+                // TODO Stop focusing any objects
+            }
+        }
+    }
+}
