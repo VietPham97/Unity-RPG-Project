0979b3a9d042c68f97f924d1848353cd1a297518
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-08-10 13:43:04 -0400

    Import character graphic and animation from Sebastian Lague.

---
+++ RPG Project/Assets/Player/CharacterAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour 
+{
+    const float locomotionAnimationSmoothTime = 0.1f;
+
+    NavMeshAgent agent;
+	Animator animator;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+    }
+}
