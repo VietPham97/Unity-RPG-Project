8891ad14f7a945ae89ef2d670fc85bfeb5b69288
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-08-23 23:07:59 -0400

    Collect items and make it appear on the InventoryUI.

--- RPG Project/Assets/Item/InventoryUI.cs
+++ RPG Project/Assets/Item/InventoryUI.cs
@@ -2,16 +2,32 @@
 
 public class InventoryUI : MonoBehaviour 
 {
+    public Transform itemsParent;
+
     Inventory inventory;
+
+    InventorySlot[] slots;
 
     private void Start()
     {
         inventory = Inventory.Instance;
         inventory.onItemChangedCallback += UpdateUI;
+
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
     }
 
     void UpdateUI()
     {
-        Debug.Log("Updating UI");
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory.items.Count)
+            {
+                slots[i].AddItem(inventory.items[i]);
+            }
+            else // out of items to add
+            {
+                slots[i].ClearSlot();
+            }
+        }
     }
 }
