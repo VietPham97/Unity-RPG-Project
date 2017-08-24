d4f65dd3a4a943e4b2191d0703b8ce3f70993c43
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-08-23 23:28:14 -0400

    Interact with the InventoryUI only. Player will not be affected by mouse clicking.

--- RPG Project/Assets/Player/PlayerController.cs
+++ RPG Project/Assets/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour 
@@ -17,6 +18,10 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        // left mouse button
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -30,6 +35,7 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+        // right mouse button
 		if (Input.GetMouseButtonDown(1))
 		{
 			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
