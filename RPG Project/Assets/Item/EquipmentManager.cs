00b4d6444dd960025196fdfe7ae753b9e2fa44da
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-08-30 17:10:06 -0400

    Create different types of equipment slot and remove it from the inventory when it is equipped.

---
+++ RPG Project/Assets/Item/EquipmentManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour 
+{
+    #region Singleton
+    public static EquipmentManager Instance
+    {
+        get;
+        private set;
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+
+    Equipment[] currentEquipment;
+
+    private void Start()
+    {
+        var numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length; // Get the length of EquipmentSlot enum
+        currentEquipment = new Equipment[numSlots]; // create a new array of type Equipment
+    }
+
+    public void Equip(Equipment newItem)
+    {
+        int slotIndex = (int)newItem.equipSlot; // get the index of the equiped item or newItem in EquipmentSlot enum
+
+        currentEquipment[slotIndex] = newItem; // set the currentEquipment to the newItem
+    }
+}
