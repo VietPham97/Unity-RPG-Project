a170330be262ca048cadcb3cbd3a3f8fce77bac8
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-09-14 18:11:57 -0400

    Implement player stats. Add damage and armour modifiers after equipping items.

---
+++ RPG Project/Assets/Player/Stats/CharacterStats.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class CharacterStats : MonoBehaviour 
+{
+    public int maxHealth = 100;
+    public int CurrentHealth { get; private set; }
+
+    public Stat damage;
+    public Stat armor;
+
+    private void Awake()
+    {
+        CurrentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        damageAmount -= armor.GetValue();
+        damageAmount = Mathf.Clamp(damageAmount, 0, int.MaxValue);
+
+        CurrentHealth -= damageAmount;
+        Debug.Log(transform.name + " takes " + damageAmount + " damage.");
+
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    /// <summary>
+    /// Die in some way.
+    /// This method is meant to be overwritten.
+    /// </summary>
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + " died.");
+    }
+}
---
+++ RPG Project/Assets/Player/Stats/PlayerStats.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    void Start()
+    {
+        EquipmentManager.Instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
+    {
+        if (newItem != null)
+        {
+			armor.AddModifier(newItem.armorModifier);
+			damage.AddModifier(newItem.damageModifier);
+        }
+
+        if (oldItem != null)
+        {
+            armor.RemoveModifier(oldItem.armorModifier);
+            damage.RemoveModifier(oldItem.damageModifier);
+        }
+    }
+}
