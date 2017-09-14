0b52381758c558310451960462ec6405700d16dd
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-09-14 17:32:09 -0400

    Replace item placeholders on scene with the real meshes.

---
+++ RPG Project/Assets/Helper Scripts/ConvertToRegularMesh.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ConvertToRegularMesh : MonoBehaviour 
+{
+    [ContextMenu("Convert to regular mesh")]
+    void Convert()
+    {
+        var skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        var meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        var meshFilter = gameObject.AddComponent<MeshFilter>();
+
+        meshFilter.sharedMesh = skinnedMeshRenderer.sharedMesh;
+        meshRenderer.sharedMaterials = skinnedMeshRenderer.sharedMaterials;
+
+        DestroyImmediate(skinnedMeshRenderer);
+        DestroyImmediate(this);
+    }
+}
