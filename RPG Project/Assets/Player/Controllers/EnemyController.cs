1ca0c38dae31e99136b4881ceaa2072503f5e1d2
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-09-17 17:08:33 -0400

    Once detected player, enemy will keep facing and chasing.

--- RPG Project/Assets/Player/Controllers/EnemyController.cs
+++ RPG Project/Assets/Player/Controllers/EnemyController.cs
@@ -1,6 +1,45 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour 
 {
+    public float lookRadius = 10f;
 
+    Transform target;
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        target = PlayerManager.Instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        var distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                // TODO Attack the target
+                FaceTarget();
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
 }
---
+++ RPG Project/Assets/Player/PlayerManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static PlayerManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    #endregion
+
+    public GameObject player;
+}
