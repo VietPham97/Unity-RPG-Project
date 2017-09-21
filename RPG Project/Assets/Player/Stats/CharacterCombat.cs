b08e3749c8b2e7900b9dc96eb9a354985b3e75ba
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-09-21 16:49:26 -0400

    Implement delay attack system. Prepare for OnAttack effect by using an event.

--- RPG Project/Assets/Player/Stats/CharacterCombat.cs
+++ RPG Project/Assets/Player/Stats/CharacterCombat.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 [RequireComponent(typeof(CharacterStats))]
 public class CharacterCombat : MonoBehaviour 
 {
     public float attackSpeed = 1f;
     float attackCooldown;
+
+    public float attackDelay = 0.6f;
+
+    public event System.Action OnAttack;
 
     CharacterStats myStats;
 
@@ -22,8 +27,20 @@ public class CharacterCombat : MonoBehaviour
     {
         if (attackCooldown <= 0)
         {
-			targetStats.TakeDamage(myStats.damage.GetValue());
+            StartCoroutine(DoDamage(targetStats, attackDelay));
+
+            if (OnAttack != null)
+            {
+                OnAttack();
+            }
+
             attackCooldown = 1f / attackSpeed;  // Reset the attackCooldown, the greater the attackSpeed, the smaller the attackCooldown
         }
+    }
+
+    IEnumerator DoDamage (CharacterStats stats, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        stats.TakeDamage(myStats.damage.GetValue());
     }
 }
